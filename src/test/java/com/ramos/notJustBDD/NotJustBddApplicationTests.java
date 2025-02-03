6e61eaa4222114dd6c0cab3a57b3f074485efcba
Carlos Torrillas Ramos <torriramos@carloss-mbp.mynet>
2025-02-03 14:59:07 +0000

    Initial commit

---
+++ src/test/java/com/ramos/notJustBDD/NotJustBddApplicationTests.java
@@ -0,0 +1,13 @@
+package com.ramos.notJustBDD;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class NotJustBddApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
